686641d78657c83e8f33af7db830dd68bc0a3265
MarioManri98 <1dawm161712@gmail.com>
2016-12-02 10:47:09 +0100

    Initial State of the Project

---
+++ src/org/japo/java/app/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.app;
+
+/**
+ *
+ * @author Mario Manrique - 1dawm161712@gmail.com
+ */
+public class App {
+    public void launchApp() {
+        
+    }
+}
